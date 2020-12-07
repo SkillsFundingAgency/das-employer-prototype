343624e6d1d12a269673adee302f2fd40ae35a27
Gary Carr <imgarycarr@gmail.com>
2020-12-07 14:36:03 +0000

    Saved standards and trainign providers

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+//SAVED STANDARDS
+
+router.post('/saved-standards/choose-provider', (req, res, next) => {
+	if(req.session.data['foundProvider'] == 'true'){
+		res.redirect('/saved-standards/choose-provider')
+	} else {
+		res.redirect('/saved-standards/advertise/selected-no')
+	}
+})
+
+
 module.exports = router
+
