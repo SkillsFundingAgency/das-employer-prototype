c6b773fae1c98ece76616e6d99c9f1ea29028c19
briananddigital <86655162+BrianMilman@users.noreply.github.com>
2021-07-23 14:37:50 +0100

    set up links for ticket 334

--- app/routes.js
+++ app/routes.js
@@ -15,5 +15,19 @@ router.post('/saved-standards/choose-provider', (req, res, next) => {
 })
 
 
+//START TERMS OF USE
+
+router.post(`/334/terms-of-use-branch`, function (req, res) {
+    const termsOfUse = req.session.data['terms-of-use'];
+
+    if (termsOfUse === 'read-now'
+       ) {
+        res.redirect(`/334/updated-terms-and-conditions`);
+    }
+   else res.redirect(`/334/dashboard-with-banner`);
+});
+
+//END TERMS OF USE
+
 module.exports = router
 
