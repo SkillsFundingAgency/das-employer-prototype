2b6ba59274ed4b6d01d100fa449bf8a8584ac534
briananddigital <86655162+BrianMilman@users.noreply.github.com>
2021-07-26 13:23:33 +0100

    Created compulsory journey for t's and c's

--- app/routes.js
+++ app/routes.js
@@ -17,14 +17,14 @@ router.post('/saved-standards/choose-provider', (req, res, next) => {
 
 //START TERMS OF USE
 
-router.post(`/334/terms-of-use-branch`, function (req, res) {
+router.post(`/334/optional/terms-of-use-branch`, function (req, res) {
     const termsOfUse = req.session.data['terms-of-use'];
 
     if (termsOfUse === 'read-now'
        ) {
-        res.redirect(`/334/updated-terms-and-conditions`);
+        res.redirect(`/334/optional/updated-terms-and-conditions`);
     }
-   else res.redirect(`/334/select-account-with-banner`);
+   else res.redirect(`/334/optional/select-account-with-banner`);
 });
 
 //END TERMS OF USE
