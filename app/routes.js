6dbda6b08359b35e0faa8965ffb5a0680a0ec421
SamNomensa <samantha.thompson@education.gov.uk>
2024-09-19 14:23:38 +0100

    levy journey with fundings, update to index

--- app/routes.js
+++ app/routes.js
@@ -2417,3 +2417,69 @@ router.post('/employer/v8/add/action/approve-details', function (req, res) {
     res.redirect('/employer/v8/add/apprentice-sent')
   }
 })
+
+
+// Employer V9 - Employer initiates journey  Routes
+
+router.post('/employer/v9/add/action/confirm-provider-details', function (req, res) {
+  var useProvider = req.session.data['use-provider'];
+
+  if (useProvider === "No") {
+    res.redirect('/employer/v9/add/provider-details');
+  } else {
+    res.redirect('/employer/v9/add/start-adding-apprentices');
+  }
+});
+
+router.post('/employer/v9/add/action/start-adding-apprentices', function (req, res) {
+  var selection = req.session.data['selection'];
+
+  if (selection === "Yes") {
+    // Redirect to page where user will add apprentice details
+    res.redirect('/employer/v9/add/apprenticeship-training-course');
+  } else if (selection === "No") {
+    // Redirect to page where user wants training provider to add apprentice details
+    res.redirect('/employer/v9/add/apprentice-request-sent');
+  } else {
+    // Handle any other scenario if needed
+    res.redirect('/employer/v9/add/action/start-adding-apprentices'); // Redirect to form page again if necessary
+  }
+});
+
+router.post('/employer/v9/add/action/approve-details', function (req, res) {
+  var selection = req.session.data['selection']
+
+  if (selection == "Send") {
+    res.redirect('/employer/v9/add/index')
+  }
+  else {
+    res.redirect('/employer/v9/add/apprentice-sent')
+  }
+})
+
+// Employer V9 - Employer initiates journey  Routes
+
+router.post('/employer/v10/add/action/confirm-provider-details', function (req, res) {
+  var useProvider = req.session.data['use-provider'];
+
+  if (useProvider === "No") {
+    res.redirect('/employer/v10/add/provider-details');
+  } else {
+    res.redirect('/employer/v10/add/start-adding-apprentices');
+  }
+});
+
+router.post('/employer/v10/add/action/start-adding-apprentices', function (req, res) {
+  var selection = req.session.data['selection'];
+
+  if (selection === "Yes") {
+    // Redirect to page where user will add apprentice details
+    res.redirect('/employer/v10/add/apprenticeship-training-course');
+  } else if (selection === "No") {
+    // Redirect to page where user wants training provider to add apprentice details
+    res.redirect('/employer/v10/add/apprentice-request-sent');
+  } else {
+    // Handle any other scenario if needed
+    res.redirect('/employer/v10/add/action/start-adding-apprentices'); // Redirect to form page again if necessary
+  }
+});
